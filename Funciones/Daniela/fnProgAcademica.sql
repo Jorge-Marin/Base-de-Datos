f25b1f13da89c16db6394b485d9c638a1ad3bc89
Daniela-2017 <51520588+Daniela-2017@users.noreply.github.com>
2020-05-10 22:45:05 -0600

    concluida

--- Funciones/Daniela/fnProgAcademica.sql
+++ Funciones/Daniela/fnProgAcademica.sql
@@ -31,13 +31,15 @@ RETURNS TABLE
 AS
 
 RETURN
-	
+
 	SELECT D.nombreDepartamento 'Departamento', 
 			A.nombreAsignatura 'Asignatura',
 			S.diaPresenciales 'Dias', 
 			S.codSeccion 'Sección', 
 			S.horaInicial 'HoraInicial',
-			S.horaFinal 'HoraFinal'
+			S.horaFinal 'HoraFinal',
+			Pe.periodo 'Periodo',
+			YEAR(Pe.fechaInicio) 'Año'
 		FROM Registro.smregistro.DepartamentosCarrera AS D
 			INNER JOIN Registro.smregistro.Asignatura AS A
 				ON D.codDepartamento = A.codDepartamentoFF
@@ -45,6 +47,19 @@ RETURN
 				ON A.codAsignatura = S.codAsignatura
 			INNER JOIN Registro.smregistro.CarreraDepartamento AS C
 				ON D.codDepartamento = C.codDepartamentoFF
+			INNER JOIN Registro.smregistro.Periodo AS Pe
+				ON Pe.codPeriodo = S.codPeriodo
 			WHERE C.codCarreraFF = @codCarrera
+				AND Pe.activo = 1
+				/*
+					Suponiendo que la programación académica está disponible 15 dias antes de iniciar matricula
+				*/
+				AND DATEDIFF(DAY,GETDATE(),Pe.inicioPrematricula) <=15
 
+				/*
+					Suponiendo que el dia de inicio del periodo se desactiva la visualización de la programación académica
+				*/
+				AND GETDATE() <= Pe.fechaInicio
 GO
+
+--SELECT * FROM smregistro.FnProgramacionAcademica ('IS01')
